0f524183d5008b8860d065e72843027dc488ee7b
abdalla440 <ahannora440@gmail.com>
2024-07-20 23:11:50 +0300

    test the sonar job only final

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from pymongo.mongo_client import MongoClient
 from bson.objectid import ObjectId
 from flask_cors import CORS
-from cryptography.fernet import Fernet
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -10,7 +9,7 @@ CORS(app, resources={r"/*": {"origins": "*", "send_wildcard": "False"}}) # Compl
 app.config['WTF_CSRF_ENABLED'] = False # Sensitive
 
 # MongoDB URI
-mongo_uri = "gAAAAABmnBZMSaeGsjpLdn4TeYR5MKdttZxTpDCOhxKatdi1-J5lYAL7h_O30VLCE_y3AiAyCcSHIIHy-tqSJ_V_dHrkL3YesiqcHHaPvsZwd_1xIItJZv29GuGty6W0PfCAkpAQpVaoKHybR4lAL6BwPKB_wJh0lV-bO2rtrUPdwxe5MF_GqA8cnlfqcuWZC5ZGuKPf1zlEtOT_j-8Rf9R1wzJo9KdjkttX_u3LF22g1yzybdpx03U="
+mongo_uri = "mongodb+srv://ahannora:AdminMONGO123@notes-cluster.erkitpg.mongodb.net/?retryWrites=true&w=majority&appName=notes-Cluster"
 client = MongoClient(mongo_uri)
 db = client['notes']
 collection = db['test']
